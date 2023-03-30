8c93060316bdc95dfcc6f6a333f10fa3191db740
jakaria455173 <jakaria455173@gmail.com>
2023-03-30 22:50:06 +0600

    Project File Structure Setups

---
+++ src/Components/BlogPost/BlogPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogPost = () => {
+  return (
+    <div>
+      <h3>Blog Post Components</h3>
+    </div>
+  );
+};
+
+export default BlogPost;
---
+++ src/Components/SpendTime/SpendTime.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SpendTime = () => {
+  return (
+    <div>
+      <h2>Post Spend Time Components</h2>
+    </div>
+  );
+};
+
+export default SpendTime;
